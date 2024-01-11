1d2a11e649fe0ebe710de44e0e3b15f449fce885
SatishJ <SatishJ@AARUSHJ>
2024-01-11 12:31:51 +0530

    Add project files.

---
+++ Aspnet Core Identity/Models/AuthDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Aspnet_Core_Identity.Models
+{
+    public class AuthDbContext : IdentityDbContext
+{
+    public AuthDbContext(DbContextOptions<AuthDbContext> options) : base(options)
+    {
+    }
+
+    // DbSet properties and other configurations...
+}
+
+
+}
---
+++ Aspnet Core Identity/Startup.cs
@@ -0,0 +1,44 @@
+﻿using Aspnet_Core_Identity.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Aspnet_Core_Identity
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // ... other services
+
+            services.AddDbContext<AuthDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("AuthConnectionString")));
+
+            services.AddIdentity<IdentityUser, IdentityRole>()
+                .AddEntityFrameworkStores<AuthDbContext>();
+
+            // ... other configurations
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            // ... other configurations
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            // ... other middleware configurations
+        }
+    }
+}
